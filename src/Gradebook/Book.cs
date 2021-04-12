a1e6da045898c9e175f73460e504b00bd823f785
Eduardo Astaiza Sandoval <hidinge@gmail.com>
2021-04-12 10:47:03 -0500

    Función de mostrar estadisticas implementado

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -1,23 +1,41 @@
 using System;
 using System.Collections.Generic;
 
-namespace Gradebook {
-
-    class Book {
-
-        private List<double> grades; //This is a field not a variable
+namespace Gradebook 
+{
+    class Book 
+    {
+        private List<double> grades; 
 
         //Constructor
-        public Book(){
+        public Book()
+        {
             grades = new List<double>();
         }
 
-        internal void showStatistics()
+        //Shows average, max and low grade of a set of data
+        public void showStatistics()
         {
-            throw new NotImplementedException();
+            double result = 0.0;
+            double highGrade= double.MinValue;
+            double lowGrade= double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                lowGrade= Math.Min(number, lowGrade);
+                highGrade= Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The average grade is {result:N1}");
+
         }
 
-        public void addGrade(double grade){
+        public void addGrade(double grade)
+        {
             grades.Add(grade);
         }
     }
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -9,21 +9,10 @@ namespace Gradebook
         {
             Book book = new Book();
             book.addGrade(4.5);
+            book.addGrade(3.5);
+            book.addGrade(2.9);
+            book.addGrade(3.7);
             book.showStatistics();
-           
-            var grades = new List<double>() {3.4, 4.5, 4.8, 2.1};
-            grades.Add(4.9);
-            
-            var result = 0.0;
-            foreach(var grade in grades){
-                
-                result += grade;
-            }
-            result /= grades.Count;
-
-            Console.WriteLine($"The average grade is {result}");
-
-            //Console.WriteLine($"Hello, {args[0]}!");
         }
     }
 }
