4751410fd4683dc79412d46b7b73a8151167d524
Eduardo Astaiza Sandoval <hidinge@gmail.com>
2021-04-14 10:49:47 -0500

    Se crea un unit test y el código es funcional

---
+++ test/Gradebook.test/BookTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace Gradebook.test
+{
+    public class BookTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            Book book = new Book("");
+
+            book.addGrade(4.5);
+            book.addGrade(3.5);
+            book.addGrade(2.9);
+            book.addGrade(3.7);
+            
+            //act
+            var result = book.getStatistics();
+
+            //assert
+            Assert.Equal(3.65, result.average, 2);
+            Assert.Equal(2.9, result.lowGrade);
+            Assert.Equal(4.5, result.highGrade);
+        }
+    }
+}
