3c99850c09240ffb9cb4c6168aeef36aa0a3315f
Eduardo Astaiza Sandoval <hidinge@gmail.com>
2021-05-03 19:59:42 -0500

    Adding new method to typetest

--- test/Gradebook.test/TypeTest.cs
+++ test/Gradebook.test/TypeTest.cs
@@ -13,6 +13,18 @@ namespace Gradebook.test
 
            Assert.Equal("Book 1", book1.Name);
            Assert.Equal("Book 2", book2.Name);
+           Assert.NotSame(book1, book2);
+        }
+
+        public void TwoVarsCanReferenceSameObject()
+        {
+           var book1 = GetBook("Book 1");
+           var book2 = book1; 
+
+           //Evaluates if two objects points at the same object in memory
+           Assert.Same(book1, book2); 
+           //Same from above but returns true if it's true
+           Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
         Book GetBook(string name)
