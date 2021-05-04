511ce8d9383dc9025af784a7ce1428f456fb8f90
Eduardo Astaiza Sandoval <hidinge@gmail.com>
2021-05-03 19:47:32 -0500

    Modified method name

--- test/Gradebook.test/TypeTest.cs
+++ test/Gradebook.test/TypeTest.cs
@@ -6,14 +6,13 @@ namespace Gradebook.test
     public class TypeTest
     {
         [Fact]
-        public void Test1()
+        public void GetBookReturnsDifferentObjects()
         {
            var book1 = GetBook("Book 1");
            var book2 = GetBook("Book 2"); 
 
            Assert.Equal("Book 1", book1.Name);
            Assert.Equal("Book 2", book2.Name);
-        
         }
 
         Book GetBook(string name)
