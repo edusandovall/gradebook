f73aaf645dd07f31338e4da00cd56cadc8a00777
Eduardo Astaiza Sandoval <hidinge@gmail.com>
2021-05-10 10:37:25 -0500

    CSharpIsPassByValue method created.

--- test/Gradebook.test/TypeTest.cs
+++ test/Gradebook.test/TypeTest.cs
@@ -5,15 +5,7 @@ namespace Gradebook.test
 {
     public class TypeTest
     {
-        [Fact]
-        public void CanSetNameFromReference()
-        {
-          var book1 = GetBook("Book 1");
-          SetName(book1, "New Name");
 
-          Assert.Equal("New Name", book1.Name);
-        }
-        
         [Fact]
         public void GetBookReturnsDifferentObjects()
         {
@@ -23,6 +15,29 @@ namespace Gradebook.test
            Assert.Equal("Book 1", book1.Name);
            Assert.Equal("Book 2", book2.Name);
            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+          var book1 = GetBook("Book 1");
+          SetName(book1, "New Name");
+
+          Assert.Equal("New Name", book1.Name);
+        }
+
+             [Fact]
+        public void CSharpIsPassByValue()
+        {
+          var book1 = GetBook("Book 1");
+          GetBookSetName(book1, "New Name");
+
+          Assert.Equal("New Name", book1.Name);
+        }
+
+         public void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
         }
 
         public void SetName(Book book, string name)
